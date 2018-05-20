0e67f9c16b3cbd345c69f757275e6b176e5b95df
Theda Rogers <crayonwriter@gmail.com>
2018-05-19 22:16:32 -0400

    Added albums intent, onclicklistener, and findviewbyid.

--- app/src/main/java/com/example/android/themusicofyou/MainActivity.java
+++ app/src/main/java/com/example/android/themusicofyou/MainActivity.java
@@ -11,6 +11,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-        }
+
+        //Find the view to connect OnClickListener to the album view
+        TextView albumView = findViewById(R.id.albums);
+
+        //Create album intent
+        final Intent albumsIntent = new Intent(MainActivity.this, Albums.class);
+
+        //Attach OnClickListener to the view
+        albumView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(albumsIntent);
+            }
+
+        });
+
+    }
+
 }
+
