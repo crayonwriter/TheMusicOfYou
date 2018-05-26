3b7fab212c706fe84f0a2d081f8cd6c7fd01a159
Theda Rogers <crayonwriter@gmail.com>
2018-05-26 19:17:23 -0400

    Created ArrayAdapter and ListView

--- app/src/main/java/com/example/android/themusicofyou/Songs.java
+++ app/src/main/java/com/example/android/themusicofyou/Songs.java
@@ -2,7 +2,9 @@ package com.example.android.themusicofyou;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,33 +17,26 @@ public class Songs extends AppCompatActivity {
         setContentView(R.layout.activity_songs);
 
         //Create arraylist for songs and added some songs.
-        ArrayList<String> songList = new ArrayList<String>();
-        songList.add("Despacito");
-        songList.add("Bailando");
-        songList.add("X");
-        songList.add("Danza Guduro");
-        songList.add("Posé");
-        songList.add("Desnuda");
-        songList.add("El Problema");
-        songList.add("Tal Vez");
-        songList.add("Taboo");
-        songList.add("Suerte");
-        songList.add("Dimelo");
+        ArrayList<String> songs = new ArrayList<String>();
+        songs.add("Despacito");
+        songs.add("Bailando");
+        songs.add("X");
+        songs.add("Danza Guduro");
+        songs.add("Posé");
+        songs.add("Desnuda");
+        songs.add("El Problema");
+        songs.add("Tal Vez");
+        songs.add("Taboo");
+        songs.add("Suerte");
+        songs.add("Dimelo");
 
         //Find the view in the XML that we're going to amend, and assign it to a variable
         LinearLayout rootView = findViewById(R.id.rootView);
 
-        //Attempting loop code
-        for (int i=0; i<songList.size(); i++) {
-            //Construct a textview (which will be a child)
-            TextView songsView = new TextView(this);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songs);
 
-            //Set the text to show on the screen in the textview
-            songsView.setText(songList.get(i));
+        ListView songListView = findViewById(R.id.songList);
 
-            //Add the TextView as a child of the linear layout
-            rootView.addView(songsView);
-        }
-
+        songListView.setAdapter(itemsAdapter);
     }
 }
