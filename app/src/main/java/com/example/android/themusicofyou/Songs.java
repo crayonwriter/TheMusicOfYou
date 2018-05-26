88dcdeef9ef727b7a81268b1e7711aefa61aea66
Theda Rogers <crayonwriter@gmail.com>
2018-05-25 23:16:01 -0400

    Added a for loop to create a TextView for each element of the ArrayList for songs titles.

--- app/src/main/java/com/example/android/themusicofyou/Songs.java
+++ app/src/main/java/com/example/android/themusicofyou/Songs.java
@@ -2,6 +2,8 @@ package com.example.android.themusicofyou;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -25,6 +27,21 @@ public class Songs extends AppCompatActivity {
         songList.add("Taboo");
         songList.add("Suerte");
         songList.add("Dimelo");
+
+        //Find the view in the XML that we're going to amend, and assign it to a variable
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        //Attempting loop code
+        for (int i=0; i<songList.size(); i++) {
+            //Construct a textview (which will be a child)
+            TextView songsView = new TextView(this);
+
+            //Set the text to show on the screen in the textview
+            songsView.setText(songList.get(i));
+
+            //Add the TextView as a child of the linear layout
+            rootView.addView(songsView);
+        }
 
     }
 }
