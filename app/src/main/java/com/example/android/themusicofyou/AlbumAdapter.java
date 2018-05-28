6715569b3ee5ef6312adceb291eab8a6b8bdf2cb
Theda Rogers <crayonwriter@gmail.com>
2018-05-28 15:29:25 -0400

    Created Albums data, hoping for a GridView showing albums with their artists and list of songs

--- app/src/main/java/com/example/android/themusicofyou/AlbumAdapter.java
+++ app/src/main/java/com/example/android/themusicofyou/AlbumAdapter.java
@@ -11,8 +11,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class SongAdapter extends ArrayAdapter<Song> {
-    public SongAdapter(Activity context, ArrayList<Song> songs) {
+public class AlbumAdapter extends ArrayAdapter<Song> {
+    public AlbumAdapter(Activity context, ArrayList<Song> songs) {
 
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
@@ -60,3 +60,6 @@ public class SongAdapter extends ArrayAdapter<Song> {
     }
 
 }
+
+
+
--- app/src/main/java/com/example/android/themusicofyou/AlbumsActivity.java
+++ app/src/main/java/com/example/android/themusicofyou/AlbumsActivity.java
@@ -1,15 +1,44 @@
+//Make a GridView for the albums, don't forget to make number of columns in the XML
 package com.example.android.themusicofyou;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
+
+import com.example.android.themusicofyou.R;
+import com.example.android.themusicofyou.Song;
+import com.example.android.themusicofyou.SongAdapter;
+
+import java.util.ArrayList;
 
 public class AlbumsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_albums);
+        setContentView(R.layout.activity_songs);
 
-        //Make a GridView for the albums, don't forget to make number of columns in the XML
+        //Create arraylist for songs and added some songs.
+        ArrayList<Song> songs = new ArrayList<Song>();
+        songs.add(new Song("Despacito", "Luis Fonsi & Daddy Yankee", "Despacito"));
+        songs.add(new Song("Bailando", "Enrique Iglesias", "Sex and Love"));
+        songs.add(new Song("X", "Nicky Jam", "X"));
+        songs.add(new Song("Danza Kuduro", "Lucenzo", "Don Omar Presents: Meet the Orphans"));
+        songs.add(new Song("Pose", "Daddy Yankee", "Talento de Barrio"));
+        songs.add(new Song("Desnuda", "Ricardo Arjana", "Sin Daños a Terceros"));
+        songs.add(new Song("El Problema", "Ricardo Arjana", "Santo Pecado"));
+        songs.add(new Song("Tal Vez", "Ricky Martin", "Almas Del Silencio"));
+        songs.add(new Song("Taboo", "Don Omar", "Don Omar Presents: Meet the Orphans"));
+        songs.add(new Song("Suerte", "Shakira", "Laundry Service"));
+        songs.add(new Song("Dimelo", "Enrique Iglesias", " Insomniac"));
+
+
+        SongAdapter adapter = new SongAdapter(this, songs);
+
+        GridView albumGridView = findViewById(R.id.albumGrid);
+
+        albumGridView.setAdapter(adapter);
     }
 }
+
